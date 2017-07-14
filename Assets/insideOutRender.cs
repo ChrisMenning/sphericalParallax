d9753d2e6a9e8c87fe1078ac84a7e8be49e370db
ChrisMenning <chrismenning@gmail.com>
2017-07-14 10:34:15 -0500

    Add files via upload

---
+++ Assets/insideOutRender.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Linq;
+
+public class insideOutRender : MonoBehaviour {
+
+	void Start () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.triangles = mesh.triangles.Reverse().ToArray();
+	}
+}
---
+++ Assets/parallax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour {
+    public Transform self;
+    public Transform cam;
+    private float radius;
+    public float exaggerator;  // A value for modifying how pronounced the parallax effect is.
+
+	// Use this for initialization
+	void Start () {
+        radius = (self.localScale.x /2) * exaggerator;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        self.rotation = Quaternion.Euler(cam.rotation.x * -radius, cam.rotation.y * -radius, cam.rotation.z * -radius);
+    }
+}
